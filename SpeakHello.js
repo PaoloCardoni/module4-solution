76c54fbf3d35d86a540ec1add333b0a8decbaf8c
PaoloCardoni <117283976+PaoloCardoni@users.noreply.github.com>
2022-12-06 15:00:21 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -26,6 +26,7 @@
 (function (window) {
   var helloSpeaker = {};
   var speakWord = "Hello";
+  helloSpeaker.name = "Yaakov";
   helloSpeaker.speak = function () {
     console.log(speakWord + " " + helloSpeaker.name);
   }
