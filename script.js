87bac2ab948c99990935e5c9975d77972b3cf8ac
PaoloCardoni <117283976+PaoloCardoni@users.noreply.github.com>
2022-12-07 12:36:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,12 +64,10 @@ WARNING!!! WARNING!!!
 (function (window) {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var name in names) {
-    var firstLetter = names[name].toLowerCase().charAt(0);
+    var firstLetter = names[name].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
-      byeSpeaker.name = names[name];
       byeSpeaker.speak(names[name]);
     } else {
-      helloSpeaker.name = names[name];
       helloSpeaker.speak(names[name]);
     }
   }  
